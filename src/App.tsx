56b7619cdc2d2f72eb060b5f1ba8dab094451d77
AlokPrasanna <alokprasanna1104@gmail.com>
2024-07-13 14:43:06 +0530

    Defiend folder Structure

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className='w-full h-[200px] bg-slate-900'>
+      <p className='text-[25px] font-bold'>Hi</p>
+    </div>
+  )
+}
+
+export default App
